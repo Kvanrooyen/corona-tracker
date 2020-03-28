1b845bc7454f9b4942092507fc4960d146e1783a
Keagan <governor.keagan@outlook.com>
2020-03-28 00:46:13 +0000

    Feature: Search & Chore
    The HTML for the cards has been removed to make it dynamic. All countries that have cases will now appear, instead of a fixed number. Removed the search page, and added a search bar to the main page. This will allow the user to search for any country that is displayed - all countries with cases.

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,44 @@
-// Gets data from the api
-fetch("https://corona.lmao.ninja/countries/")
+fetch("https://corona.lmao.ninja/countries/?sort=cases")
   .then(function(resp) {
     return resp.json();
   }) //Converts the returned data to JSON
   .then(function(data) {
-    // Loop through and iterate over the ID for both the HTMl and the JSON
-    for (i = 0; i < 11; i++) {
-      document.getElementById("country" + [i]).innerHTML = data[i].country;
-      document.getElementById("cases" + [i]).innerHTML =
-        "Cases: " + data[i].cases;
-      document.getElementById("deaths" + [i]).innerHTML =
-        "Deaths: " + data[i].deaths;
-      document.getElementById("casesToday" + [i]).innerHTML =
-        "Cases Today: " + data[i].todayCases;
-      document.getElementById("deathsToday" + [i]).innerHTML =
-        "Deaths Today: " + data[i].todayDeaths;
-    }
+    cardBlock(data);
+    virusStats(data);
   });
+
+function cardBlock(d) {
+  noCtry = Object.keys(d).length;
+  // Loop through the number of countries with cases and add them
+  for (j = 0; j < noCtry; j++) {
+    var cardBlockCode =
+      `<li class="collection-item">` +
+      `<div class="col s12 m6">` +
+      '<div class="card blue-grey darken-1">' +
+      `<div class="card-content white-text">` +
+      `<span class="card-title" id="country${j}"></span>` +
+      `<p id="cases${j}"></p>` +
+      `<p id="deaths${j}"></p>` +
+      `<p id="casesToday${j}"></p>` +
+      `<p id="deathsToday${j}"></p>` +
+      `</div>` +
+      `</div>` +
+      `</li>`;
+    document.getElementById("cardInput").innerHTML += cardBlockCode;
+  }
+}
+
+function virusStats(d) {
+  // Loop through and iterate over the ID for both the HTMl and the JSON
+  for (i = 0; i < noCtry; i++) {
+    document.getElementById(`country${i}`).innerHTML = d[i].country;
+    document.getElementById(`cases${i}`).innerHTML = `Cases: ${d[i].cases}`;
+    document.getElementById(`deaths${i}`).innerHTML = `Deaths: ${d[i].deaths}`;
+    document.getElementById(
+      `casesToday${i}`
+    ).innerHTML = `Cases Today: ${d[i].todayCases}`;
+    document.getElementById(
+      `deathsToday${i}`
+    ).innerHTML = `Deaths Today: ${d[i].todayDeaths}`;
+  }
+}
--- src/js/search.js
+++ src/js/search.js
@@ -1,22 +1,25 @@
-function getCountry(userInput) {
-  userInput = document.getElementById("countrySearch").value;
-  userInput.replace(/\s/g, "%20");
-  countryInfo(userInput);
-}
+// Get input element
+let filterInput = document.getElementById("filterInput");
+// Add event listener
+filterInput.addEventListener("keyup", filterNames);
 
-function countryInfo(countryName) {
-  // Gets data from the api
-  fetch("https://corona.lmao.ninja/countries/" + countryName)
-    .then(function(resp) {
-      return resp.json();
-    }) //Converts the returned data to JSON
-    .then(function(data) {
-      document.getElementById("country").innerHTML = data.country;
-      document.getElementById("cases").innerHTML = "Cases: " + data.cases;
-      document.getElementById("deaths").innerHTML = "Deaths: " + data.deaths;
-      document.getElementById("casesToday").innerHTML =
-        "Cases Today: " + data.todayCases;
-      document.getElementById("deathsToday").innerHTML =
-        "Deaths Today: " + data.todayDeaths;
-    });
+function filterNames() {
+  // Get value of input
+  let filterValue = document.getElementById("filterInput").value.toUpperCase();
+
+  // Get names ul
+  let ul = document.getElementById("cardInput");
+  // Get lis from ul
+  let li = ul.querySelectorAll("li.collection-item");
+
+  // Loop through collection-item lis
+  for (let i = 0; i < li.length; i++) {
+    let a = li[i].getElementsByTagName("div")[0];
+    // If matched
+    if (a.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
 }
