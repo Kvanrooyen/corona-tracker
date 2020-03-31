d9cc1565dd0ee8e7061c02308ded984d57b8b7e3
Keagan <governor.keagan@outlook.com>
2020-03-31 20:31:09 +0100

    FEature: BG Colour and Nav
     - Removed the naviagtion bar, since it wasn't being used.
     - Changed the background/card colour to a dark colour to improve readability.
     - Increased font size to improve legability
     - Changed "Cases: ..." to "Total Cases: ..."

--- src/js/main.js
+++ src/js/main.js
@@ -14,14 +14,14 @@ function cardBlock(d) {
     var cardBlockCode =
       `<li class="collection-item">` +
       `<div class="col s12 m6">` +
-      '<div class="card blue-grey darken-1">' +
+      '<div class="card grey darken-3">' +
       `<div class="card-content white-text">` +
       `<span class="card-title" id="country${j}"><img src="${d[j].countryInfo.flag}" class="ctryFlag"/> </span>` +
-      `<span id="cases${j}"></span>` +
-      `<span id="casesToday${j}" class="casesToday"></span>` +
+      `<span id="cases${j}" class="cases"></span>` +
+      `<span id="casesToday${j}" class="casesToday cases"></span>` +
       `<br />` +
-      `<span id="deaths${j}"></span>` +
-      `<span id="deathsToday${j}" class="deathsToday"></span>` +
+      `<span id="deaths${j}" class="deaths"></span>` +
+      `<span id="deathsToday${j}" class="deathsToday deaths"></span>` +
       `</div>` +
       `</div>` +
       `</li>`;
@@ -33,13 +33,21 @@ function virusStats(d) {
   // Loop through and iterate over the ID for both the HTMl and the JSON
   for (i = 0; i < noCtry; i++) {
     document.getElementById(`country${i}`).innerHTML += d[i].country;
-    document.getElementById(`cases${i}`).innerHTML = `Cases: ${d[i].cases}`;
-    document.getElementById(`deaths${i}`).innerHTML = `Deaths: ${d[i].deaths}`;
     document.getElementById(
-      `casesToday${i}`
-    ).innerHTML = ` (+${d[i].todayCases}) `;
+      `cases${i}`
+    ).innerHTML = `Total Cases: ${formatNumber(d[i].cases)}`;
     document.getElementById(
-      `deathsToday${i}`
-    ).innerHTML = ` (${d[i].todayDeaths}) `;
+      `deaths${i}`
+    ).innerHTML = `Total Deaths: ${formatNumber(d[i].deaths)}`;
+    document.getElementById(`casesToday${i}`).innerHTML = formatNumber(
+      ` (+${d[i].todayCases}) `
+    );
+    document.getElementById(`deathsToday${i}`).innerHTML = formatNumber(
+      ` (${d[i].todayDeaths}) `
+    );
   }
+}
+
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, " ");
 }
